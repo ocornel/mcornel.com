838aa876f4bb8b8da8281c67ede027ea21f88876
Martin Cornel <mrtncornel@gmail.com>
2019-09-23 20:40:17 +0300

    Got started on web tools.

---
+++ tools/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Home of online tools and utilities - Consult mCornel</title>
+    <meta name="description" content="Useful online tools like Beautifiers, Minifiers, Converters, Utilities, Validators, Editors for users and developers."/>
+    <?php include('parts/head.html');?>
+</head>
+<body>
+<?php include('parts/header.html'); ?>
+</body>
+</html>
