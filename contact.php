245dc78f66b912c1a6683704b3e07927c1669783
Martin Cornel <mrtncornel@gmail.com>
2019-09-24 21:14:09 +0300

    Landing contact form functionality upgraded.

--- contact.php
+++ contact.php
@@ -2,15 +2,14 @@
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "mrtncornel@gmail.com";
-$subject = "Contact Form";
+$phone = $_POST['phone'];
+$subject = $_POST['subject'];
+$full_message = "From: $name \n Message: $message \n Phone: $phone";
 $mailheader = 'From: mrtncornel@gmail.com' . "\r\n" .
-    'Reply-To: ' .$email. "\r\n" .
+    'Reply-To: ' . $email . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+mail("mrtncornel@gmail.com", $subject, $full_message, $mailheader) or die("Error!");
 echo "Thank You!";
 
 ?>
-
--- tools/js/contact_us.js
+++ tools/js/contact_us.js
@@ -1,126 +1,120 @@
-function is_email_valid(email)
-{
-	regx =/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{1,}))$/;
-	valid_mail = regx.test(email);
-	if(valid_mail)
-	{
-		return true;
-	}else{
-		return false;
-	}
+function is_email_valid(email) {
+    regx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{1,}))$/;
+    valid_mail = regx.test(email);
+    if (valid_mail) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-function is_form_ok(name, email, message)
-{
-	valid = true;
-	
-	if(name == "")
-	{
-		$("p.info_out").html('<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>&nbsp;Please enter your name.').addClass("text-danger");
-		$("#name").focus().parents("div.form-group").addClass("has-error");
-		valid = false;
-		return false;
-	}else
-	{
-		$("p.info_out").html("").removeClass("text-danger");
-		$("#name").parents("div.form-group").removeClass("has-error");
-	}
+function is_form_ok(name, email, message) {
+    valid = true;
 
-	if(!is_email_valid(email))
-	{
-		$("p.info_out").html('<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>&nbsp;Please enter a valid email address.').addClass("text-danger");
-		$("#email").focus().parents("div.form-group").addClass("has-error");
-		valid = false;
-		return false;
-	}else
-	{
-		$("p.info_out").html("").removeClass("text-danger");
-		$("#email").parents("div.form-group").removeClass("has-error");
-	}
+    if (name == "") {
+        $("p.info_out").html('<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>&nbsp;Please enter your name.').addClass("text-danger");
+        $("#name").focus().parents("div.form-group").addClass("has-error");
+        valid = false;
+        return false;
+    } else {
+        $("p.info_out").html("").removeClass("text-danger");
+        $("#name").parents("div.form-group").removeClass("has-error");
+    }
 
-	if(message == "")
-	{
-		$("p.info_out").html('<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>&nbsp;Please enter your message.').addClass("text-danger");
-		$("#message").focus().parents("div.form-group").addClass("has-error");
-		valid = false;
-		return false;
-	}else
-	{
-		$("p.info_out").html("").removeClass("text-danger");
-		$("#message").parents("div.form-group").removeClass("has-error");
-	}
-	
-	// if(recaptcha == "")
-	// {
-	// 	$("p.info_out").html('<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>&nbsp;Please check the recaptcha.').addClass("text-danger");
-	// 	$("html, body").animate({
-	// 		scrollTop: $("p.info_out").offset().top - 10
-	// 	}, 500);
-	// 	valid = false;
-	// 	return false;
-	// }else
-	// {
-	// 	$("p.info_out").html("").removeClass("text-danger");
-	// }
-	
-	return valid;
+    if (!is_email_valid(email)) {
+        $("p.info_out").html('<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>&nbsp;Please enter a valid email address.').addClass("text-danger");
+        $("#email").focus().parents("div.form-group").addClass("has-error");
+        valid = false;
+        return false;
+    } else {
+        $("p.info_out").html("").removeClass("text-danger");
+        $("#email").parents("div.form-group").removeClass("has-error");
+    }
+
+    if (message == "") {
+        $("p.info_out").html('<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>&nbsp;Please enter your message.').addClass("text-danger");
+        $("#message").focus().parents("div.form-group").addClass("has-error");
+        valid = false;
+        return false;
+    } else {
+        $("p.info_out").html("").removeClass("text-danger");
+        $("#message").parents("div.form-group").removeClass("has-error");
+    }
+
+    // if(recaptcha == "")
+    // {
+    // 	$("p.info_out").html('<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>&nbsp;Please check the recaptcha.').addClass("text-danger");
+    // 	$("html, body").animate({
+    // 		scrollTop: $("p.info_out").offset().top - 10
+    // 	}, 500);
+    // 	valid = false;
+    // 	return false;
+    // }else
+    // {
+    // 	$("p.info_out").html("").removeClass("text-danger");
+    // }
+
+    return valid;
 }
 
-$(document).ready(function(e) {
-	$("#clear").click(function(e) {
-    	e.preventDefault();
-		$("#name,#email,#message").val("");
-		$("p.info_out").html("");
-		$("#name,#email,#message").parents("div.form-group").removeClass("has-error");
+$(document).ready(function (e) {
+    $("#clear").click(function (e) {
+        e.preventDefault();
+        $("#name,#email,#message").val("");
+        $("p.info_out").html("");
+        $("#name,#email,#message").parents("div.form-group").removeClass("has-error");
     });
-	
-	$("#send").click(function(e) {
-    	e.preventDefault();
-		
-		name = $.trim($("#name").val());
-		email = $.trim($("#email").val());
-		message = $.trim($("#message").val());
 
-		if(!is_form_ok(name, email, message))
-		{
-			return false;
-		}
-		$("p.info_out").html('<span class="glyphicon glyphicon-hourglass" aria-hidden="true"></span>&nbsp;Please wait while sending your message.').addClass("text-info");
-		$("html, body").animate({
-			scrollTop: $("p.info_out").offset().top - 10
-		}, 500);
-		$.ajax({
-			type	: "POST",
-			url		: "../contact.php",
-			data	: {"name" : name, "email" : email, "message" : message},
-			success : function(data)
-			{
-				console.log(data);
-				$("p.info_out").html('<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>&nbsp;Your message has been sent.').addClass("text-success");
-				$("html, body").animate({
-					scrollTop: $("p.info_out").offset().top - 10
-				}, 500);
-				$("#name,#email,#message").val("");
-				$("#name,#email,#message").parents("div.form-group").removeClass("has-error");
+    $("#send").click(function (e) {
+        e.preventDefault();
 
-				// if(data === "Thank You!")
-				// {
-				//
-				// }else
-				// {
-				// 	$("p.info_out").html('<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>&nbsp;' + data).addClass("text-danger");
-				// 	$("html, body").animate({
-				// 		scrollTop: $("p.info_out").offset().top - 10
-				// 	}, 500);
-				// }
-			},
-			error 	: function()
-			{
-				$("p.info_out").html('<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>&nbsp;Connection lost! Please try again.').addClass("text-danger");
-				$("html, body").animate({
-					scrollTop: $("p.info_out").offset().top - 10
-				}, 500);
-			}
-		});
+        name = $.trim($("#name").val());
+        email = $.trim($("#email").val());
+        message = $.trim($("#message").val());
+
+        if (!is_form_ok(name, email, message)) {
+            return false;
+        }
+        $("p.info_out").html('<span class="glyphicon glyphicon-hourglass" aria-hidden="true"></span>&nbsp;Please wait while sending your message.').addClass("text-info");
+        $("html, body").animate({
+            scrollTop: $("p.info_out").offset().top - 10
+        }, 500);
+        $.ajax({
+            type: "POST",
+            url: "../contact.php",
+            data: {
+                "name": name,
+                "email": email,
+                "message": message,
+                "subject": "Contact Form",
+                "phone": "Unset",
+            },
+            success: function (data) {
+                console.log(data);
+                $("p.info_out").html('<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>&nbsp;Your message has been sent.').addClass("text-success");
+                $("html, body").animate({
+                    scrollTop: $("p.info_out").offset().top - 10
+                }, 500);
+                $("#name,#email,#message").val("");
+                $("#name,#email,#message").parents("div.form-group").removeClass("has-error");
+
+                // if(data === "Thank You!")
+                // {
+                //
+                // }else
+                // {
+                // 	$("p.info_out").html('<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>&nbsp;' + data).addClass("text-danger");
+                // 	$("html, body").animate({
+                // 		scrollTop: $("p.info_out").offset().top - 10
+                // 	}, 500);
+                // }
+            },
+            error: function () {
+                $("p.info_out").html('<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>&nbsp;Connection lost! Please try again.').addClass("text-danger");
+                $("html, body").animate({
+                    scrollTop: $("p.info_out").offset().top - 10
+                }, 500);
+            }
+        });
     });
 });
