8521f1a98475576146cd60f3185056601884268b
Martin Cornel <mrtncornel@gmail.com>
2019-10-29 14:27:27 +0300

    Clear cart

---
+++ ecom/clear_cart.php
@@ -0,0 +1,15 @@
+<?php
+include("ecom_connection.php");
+include('getOneFunc.php');
+$user_id = $_GET["user_id"];
+
+$user = getOne('users', $user_id);
+if ($user != null) {
+    $querry = $connection->prepare("delete from cart where user_id=$user_id");
+    $querry->execute();
+    echo("Cart cleared successfully");
+} else {
+    echo "Missed user";
+}
+
+
